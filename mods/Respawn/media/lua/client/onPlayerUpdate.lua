5308139cd395cb203c1e1d14bcddd60b4965d336
WobbyChip <wobbychip.simik@gmail.com>
2022-01-29 02:00:07 +0200

    Update onPlayerUpdate.lua

--- mods/Respawn/media/lua/client/onPlayerUpdate.lua
+++ mods/Respawn/media/lua/client/onPlayerUpdate.lua
@@ -1,6 +1,6 @@
 function onPlayerUpdate(player)
     if player:getHealth() > 0 and player:getBodyDamage():getHealth() > 0 then return end
-    if Respawn.bDead  then return end
+    if Respawn.bDead then return end
 
     local isMe = (player:getOnlineID() == getPlayer():getOnlineID());
     if not isMe then return end
