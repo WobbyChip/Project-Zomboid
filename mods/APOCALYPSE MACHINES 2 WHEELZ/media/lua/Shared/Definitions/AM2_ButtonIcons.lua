276c753c84c9716ee0a19cc5f543b8e91655b861
WobbyChip <wobbychip.simik@gmail.com>
2022-01-31 20:07:58 +0200

    1

--- mods/APOCALYPSE MACHINES 2 WHEELZ/media/lua/Shared/Definitions/AM2_ButtonIcons.lua
+++
@@ -1,4 +0,0 @@
-require "Definitions/ContainerButtonIcons"
-
-ContainerButtonIcons.TruckBed1 = getTexture("media/textures/Item_Duffelbag.png")
-ContainerButtonIcons.TruckBed2 = getTexture("media/textures/Item_Duffelbag.png")
