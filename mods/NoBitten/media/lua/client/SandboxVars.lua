d2e34a22dd285dde08efba026dfa4a8ba12f0f3e
WobbyChip <wobbychip.simik@gmail.com>
2022-02-04 20:02:17 +0200

    1

--- mods/NoBitten/media/lua/client/SandboxVars.lua
+++
@@ -1,7 +0,0 @@
-if SandboxVars.bittenHealthPenalty == nil then
-    SandboxVars.bittenHealthPenalty = 20;
-end
-
-if SandboxVars.bittenUpdateInterval == nil then
-    SandboxVars.bittenUpdateInterval = 0.5;
-end
---
+++ mods/NoBitten/media/lua/server/Items/NoBitten_ProceduralDistributions.lua
@@ -0,0 +1,8 @@
+--if (Rand.Next(10000) <= chance * 100.0f * getLootModifier(itemName) + n * 10.0f) {
+--getLootModifier[custom item] -> 0.6; n -> ?
+
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, "NoBitten.Antibodies");
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, 1.0);
+
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "NoBitten.Antibodies");
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, 1.0);
